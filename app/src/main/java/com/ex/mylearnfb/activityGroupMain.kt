0d2ae4ead1533a77d7d9c87b708c64036ad80735
pvoipt <30494557+pvoipt@users.noreply.github.com>
2020-11-15 04:23:53 +0900

    그룹 목록 표시 기능 완성, 그룹 내부 activityGroupMain 제작 중

---
+++ app/src/main/java/com/ex/mylearnfb/activityGroupMain.kt
@@ -0,0 +1,11 @@
+package com.ex.mylearnfb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activityGroupMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_main)
+    }
+}
